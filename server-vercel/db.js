43683bb80bdd1f539c013dfdeac2f2c2a99e29d4
Naveen <notnaveen2@gmail.com>
2025-01-08 18:00:27 +0530

    updated mongoURL

--- server-vercel/db.js
+++ server-vercel/db.js
@@ -1,6 +1,8 @@
 import mongoose, { mongo } from "mongoose";
-const MONGODB_URL = process.env.MONGODBURL;
-mongoose.connect(MONGODB_URL);
+//const MONGODB_URL = process.env.MONGODBURL;
+mongoose.connect(
+  "mongodb+srv://notnaveen2:HNPQx2348kocn8N6@cluster0.ncy9o.mongodb.net/codeDrop?retryWrites=true&w=majority&appName=Cluster0/codedrop"
+);
 
 const codeDropSchema = new mongoose.Schema({
   code: String,
