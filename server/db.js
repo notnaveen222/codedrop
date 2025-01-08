c7480ba1a03aa0730b26cd06135a6ad82ec016e7
Naveen <notnaveen2@gmail.com>
2025-01-08 15:41:20 +0530

    added mongoURL to env

--- server/db.js
+++ server/db.js
@@ -1,8 +1,6 @@
 import mongoose, { mongo } from "mongoose";
-
-mongoose.connect(
-  "mongodb+srv://notnaveen2:HNPQx2348kocn8N6@cluster0.ncy9o.mongodb.net/codeDrop?retryWrites=true&w=majority&appName=Cluster0/codedrop"
-);
+const MONGODB_URL = process.env.MONGODBURL;
+mongoose.connect(MONGODB_URL);
 
 const codeDropSchema = new mongoose.Schema({
   code: String,
